a3612f6a7608ca90a2d76ed3cd426934b8e6b3c8
Ysj <543501451@qq.com>
2023-08-25 01:00:23 +0800

    refactor：独立模块实现 aspect 字节码修改器
    (cherry picked from commit 77a56ba751c0a7e77ea42b92119cc20945f08e65)

---
+++ lib_modifier_aspect/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java-library")
+    id("kotlin")
+}
+
+group = LIB_GROUP_ID
+version = LIB_VERSION
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    implementation(project("aspect-api"))
+    implementation(project(":lib_bcu_plugin:plugin-api"))
+    compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$KOTLIN_VERSION")
+    compileOnly("org.ow2.asm:asm:9.2")
+    implementation("org.ow2.asm:asm-tree:6.2")
+    implementation("org.ow2.asm:asm-util:6.2")
+}
+
+mavenPublish(LIB_GROUP_ID, LIB_VERSION)
